cb4185cf1732b6d28aa38d2b0a2613a0d9eccc29
aalmalood <almalood.91@gmail.com>
2024-05-21 01:35:32 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // Import the necessary Firebase functions
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js";
 import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-analytics.js";
-import { getAuth, signInAnonymously, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-auth.js";
+import { getAuth, signInAnonymously } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-auth.js";
 import { getDatabase, ref, set, get, child, update } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-database.js";
 
 // Your web app's Firebase configuration
@@ -19,8 +19,8 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
-const db = getDatabase(app);
 const auth = getAuth(app);
+const db = getDatabase(app);
 
 const profileSelect = document.getElementById("profile");
 const newProfileInput = document.getElementById("new-profile");

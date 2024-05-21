dd5411ff4b8bbbd94ed3fe4e6318f3d9ba69f29a
aalmalood <almalood.91@gmail.com>
2024-05-21 15:33:15 +0000

    sss
    Signed-off-by: aalmalood <almalood.91@gmail.com>

--- admin-script.js
+++ admin-script.js
@@ -220,20 +220,29 @@ function loadLogs(profile) {
             const logs = snapshot.val();
             const logsList = document.getElementById("log-list");
             logsList.innerHTML = '';
-            const div = document.createElement('div');
-            div.classList.add('list-group-item');
-            const datee = new Date(logs.date).toLocaleString()
-            div.innerHTML = `
-                <div class="d-flex justify-content-between align-items-center">
-                <label>date: ${datee}</label>
-                <label>exercise: ${logs.exercise}</label>
-                <label>currentCount: ${logs.currentCount}</label>
-                <label>newCount: ${logs.newCount}</label>
-                <label>reduced: ${logs.reduced}</label>
-                </div>
-            `;
-            
-            logsList.appendChild(div);
+            for (let logKey in logs) {
+                const log = logs[logKey]; // Access each log object
+               
+                // Check if properties exist, if not, assign an empty string
+                const date = log.date ? log.date : '';
+                const exercise = log.exercise ? log.exercise : '';
+                const currentCount = log.currentCount ? log.currentCount : '';
+                const reduced = log.reduced ? log.reduced : '';
+                const newCount = log.newCount ? log.newCount : '';
+
+                const div = document.createElement('div');
+                div.classList.add('list-group-item');
+                div.innerHTML = `
+                    <div class="d-flex justify-content-between align-items-center">
+                        <label>${log}</label>
+                        <label>${exercise}</label>
+                        <label>${currentCount}</label>
+                        <label>${reduced}</label>
+                        <label>${newCount}</label>
+                    </div>
+                `;
+                logsList.appendChild(div);
+            }
         } else {
             console.log("No logs available for this profile");
         }

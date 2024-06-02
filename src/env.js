4de0ab69eba15c2259d567c7d944d312e59bd2a7
aalmalood <almalood.91@gmail.com>
2024-06-02 09:13:09 +0300

    Add files via upload

---
+++ src/env.js
@@ -0,0 +1,11 @@
+export const environment = {
+    apiKey: "AIzaSyCVG033ZZhIjMky7Zb2PqsdJb02ay5Fme4",
+    authDomain: "excersicetracker.firebaseapp.com",
+    databaseURL: "https://excersicetracker-default-rtdb.firebaseio.com",
+    projectId: "excersicetracker",
+    storageBucket: "excersicetracker.appspot.com",
+    messagingSenderId: "973586629666",
+    appId: "1:973586629666:web:15f6e3e0b1d43ac040f67b",
+    measurementId: "G-L3M00731C1"
+
+  };
